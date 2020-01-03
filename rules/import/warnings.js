65965a44e8b84bbf30e55f11d1727f274f323d87
mrewers <mrewers@users.noreply.github.com>
2020-01-03 10:41:51 -0500

    Tweak import/no-extraneous-dependencies rule

--- rules/import/warnings.js
+++ rules/import/warnings.js
@@ -2,7 +2,14 @@ module.exports = {
   rules: {
     'import/export': 'error',
     'import/no-deprecated': 'error',
-    'import/no-extraneous-dependencies': 'error',
+    'import/no-extraneous-dependencies': [
+      'warn',
+      {
+        devDependencies: true,
+        optionalDependencies: true,
+        peerDependencies: true
+      }
+    ],
     'import/no-mutable-exports': 'error',
     'import/no-named-as-default': 'error',
     'import/no-named-as-default-member': 'error',
