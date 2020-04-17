4b719c18e1a655c108f8d36f234336ec96fa1049
mrewers <mrewers@users.noreply.github.com>
2020-04-17 15:00:10 -0400

    Create shared config file and import it into default config along with Prettier replacement rules

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
-const rules = [
-  './rules/base/bestPractices',
-  './rules/base/errors',
-  './rules/base/es6',
-  './rules/base/nodeCommon',
-  './rules/base/strict',
-  './rules/base/stylistic',
-  './rules/base/variables',
-  './rules/import/index.js',
-  './rules/prettier/configurable',
-  './rules/prettier/core'
-].map( require.resolve );
+const rules = ['./shared', './rules/prettier/configurable', './rules/prettier/no-prettier'].map(
+  require.resolve
+);
 
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-    jquery: true,
-    node: true,
-    serviceworker: true
-  },
-  extends: rules,
-  parserOptions: {
-    ecmaFeatures: {
-      impliedStrict: true
-    },
-    ecmaVersion: 2020,
-    sourceType: 'module'
-  },
-  rules: {
-    // Prettier overrides:
-    'arrow-parens': ['error', 'as-needed'],
-    'object-curly-spacing': ['error', 'always'],
-    'space-in-parens': ['warn', 'always']
-  }
+  extends: rules
 };
---
+++ shared.js
@@ -0,0 +1,29 @@
+const rules = [
+  './rules/base/bestPractices',
+  './rules/base/errors',
+  './rules/base/es6',
+  './rules/base/nodeCommon',
+  './rules/base/strict',
+  './rules/base/stylistic',
+  './rules/base/variables',
+  './rules/import/index.js',
+].map( require.resolve );
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    jquery: true,
+    node: true,
+    serviceworker: true
+  },
+  extends: rules,
+  parserOptions: {
+    ecmaFeatures: {
+      impliedStrict: true
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  }
+};
